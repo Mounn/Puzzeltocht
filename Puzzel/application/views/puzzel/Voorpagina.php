a4231b7c929c5fb00f43b273907da2c1bbdfbff9
MOUNIR\Mounir <mounn_mocro@hotmail.com>
2016-10-31 23:20:09 +0100

    New css ; en Nieuwe voorpagina voor puzzeltocht > mounn

---
+++ Puzzel/application/views/puzzel/Voorpagina.php
@@ -0,0 +1,24 @@
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Dosis:500' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" type="text/css" href="voorpagina.css">
+</head>
+
+<body>
+  <h1>Puzzeltocht Main Menu</h1>
+  
+
+  <div id="menu">
+    <div class="buffer"></div>
+    <h2>Main Menu</h2>
+    <ul>
+      <li><a href="Puzzeltocht">Spelen<span class="trim"></span></a></li><br>
+      <li><a href="Account">Mijn account<span class="trim"></span></a></li><br>
+      <li><span class="trim"></span>Scoreboard</li><br>
+      <li><span class="trim"></span>Opties</li><br>
+      <li><span class="trim"></span>Extras</li>
+    </ul>
+    
+  </div>
+  
+  
+</body>
